e8e7cfc0599830783fc35f71ab66f94a63bcaa1d
dutreras <dutreras369@gmail.com>
2023-01-09 18:59:32 -0300

    Initial commit base

---
+++ base-backend/src/test/java/com/adareloise/base/BaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adareloise.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
